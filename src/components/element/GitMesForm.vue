7ea1bc667c08af60957077e8db039a3698c31f14
ARJUN SHAJI <arjun.shaji@techgentsia.com>
2024-01-16 22:40:33 +0530

    feat: validation adding

--- src/components/element/GitMesForm.vue
+++ src/components/element/GitMesForm.vue
@@ -1,21 +1,56 @@
 <script setup>
-    import { ref } from 'vue';
-    import { useRouter } from 'vue-router';
-    import axios from 'axios';
-    import { useApiStore } from '../store/pinia';
+import { ref, watch } from 'vue';
+import { useRouter } from 'vue-router';
+import axios from 'axios';
+import { useApiStore } from '../store/pinia';
 
-    const router = useRouter();
+const router = useRouter();
 
-    const formValues = ref({
-      secret: '',
-      author: '',
-      branch: '',
-      projectId: '',
-      since: '',
-      until: ''
+const authorError = ref(false);
+const projectIdError = ref(false);
+const completeFormError = ref(false);
+
+const formValues = ref({
+    secret: '',
+    author: '',
+    branch: '',
+    projectId: '',
+    since: '',
+    until: ''
+});
+
+function watchAndValidate(valueGetter, validationFn, errorRef) {
+    watch(() => valueGetter(), () => {
+        errorRef.value = !validationFn(valueGetter());
     });
+}
 
-    const gitCommits = async () => {
+watchAndValidate(() => formValues.value.author, isAlphaString, authorError);
+watchAndValidate(() => formValues.value.projectId, isNumericString, projectIdError);
+
+function isAnyValueEmpty(formData) {
+    for (const key in formData.value) {
+        console.log(formData[key]);
+        if (formData.value[key] === '' || formData.value[key] === undefined) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function isAlphaString(str) {
+    return /^[a-zA-Z\s]+$/.test(str);
+}
+
+function isNumericString(str) {
+    return /^[0-9]+$/.test(str);
+}
+
+const gitCommits = async () => {
+
+    if (isAnyValueEmpty(formValues)) {
+        completeFormError.value = true
+    } else {
         try {
             const queryParam = `?author=${formValues.value.author}&ref_name=${formValues.value.branch}&since=${formValues.value.since}&until=${formValues.value.until}`
             let queryUrl = `https://gitlab.techgentsia.com/api/v4/projects/${formValues.value.projectId}/repository/commits` + queryParam;
@@ -32,15 +67,27 @@
         }
         router.push('/commits')
     }
+}
 </script>
 
 <template>
     <div class="form-container">
         <form class="input-form">
+            <div v-if="completeFormError">
+                <p class="input-error-text-complete">Please fill the complete fields !</p>
+            </div>
             <input v-model="formValues.secret" placeholder="Gitlab Shared Secret" class="input-data" type="text">
-            <input v-model="formValues.author" placeholder="Author Name" class="input-data" type="text">
-            <input v-model="formValues.branch" placeholder="Project Branch Name" class="input-data" type="text">
-            <input v-model="formValues.projectId" placeholder="Project ID" class="input-data" type="text">
+            <div>
+                <input v-model="formValues.author" placeholder="Author Name" class="input-data" type="text">
+                <p v-if="authorError" class="input-error-text">Please provide a valid author name !</p>
+            </div>
+            <div>
+                <input v-model="formValues.branch" placeholder="Project Branch Name" class="input-data" type="text">
+            </div>
+            <div>
+                <input v-model="formValues.projectId" placeholder="Project ID" class="input-data" type="text">
+                <p v-if="projectIdError" class="input-error-text">Please provide a valid projectId !</p>
+            </div>
             <div class="date-container">
                 <label class="date-label" for="">Since</label>
                 <input v-model="formValues.since" class="input-data date-input" type="date">
@@ -55,82 +102,96 @@
 </template>
 
 <style scoped>
+* {
+    color: #fff;
+}
 
-    * {
-        color: #fff;
-    }
+label {
+    padding: 0;
+    margin: 0;
+}
 
-    label {
-        padding: 0;
-        margin: 0;
-    }
+.date-container {
+    position: relative;
+}
 
-    .date-container {
-        position: relative;
-    }
+.input-error-text {
+    font-size: 13px;
+    margin-bottom: 10px;
+    color: rgb(255, 187, 0);
+}
 
-    .date-label {
-        position: absolute;
-        top: -7px;
-        left: 35px;
-        font-size: 11px;
-        padding-left: 3px;
-        padding-right: 3px;
-        color: gray;
-    }
-    
-    .date-label:hover {
-        color: rgb(248, 105, 38);
-    }
+.input-error-text-complete {
+    font-size: 13px;
+    margin-bottom: 10px;
+    color: rgb(255, 187, 0);
+    text-align: center;
+}
 
-    .button {
-        padding: 0;
-        margin: 0;
-    }
-    .form-container {
-        margin-top: 20px;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        flex-direction: column;
-        row-gap: 20px;
-    }
-    .form-text {
-        font-size: 20px;
-    }
+.date-label {
+    position: absolute;
+    top: -7px;
+    left: 35px;
+    font-size: 11px;
+    padding-left: 3px;
+    padding-right: 3px;
+    color: gray;
+}
 
-    .input-form {
-        width: 400px;
-    }
+.date-label:hover {
+    color: rgb(248, 105, 38);
+}
 
-    .input-data {
-        margin-bottom: 10px;
-        width: 100%;
-        height: 40px;
-        border-color:  rgb(248, 105, 38);
-        padding-left: 15px;
-        border-radius: 6px;
-    }
+.button {
+    padding: 0;
+    margin: 0;
+}
 
-    .input-data:hover {
-        border-color: rgb(253, 81, 1);
-    }
+.form-container {
+    margin-top: 20px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    row-gap: 20px;
+}
 
-    .input-data:focus {
-        outline: none;
-    }
+.form-text {
+    font-size: 20px;
+}
 
-    .form-submit-button {
-        width: 300px;
-        height: 40px;
-        border-color:  rgb(248, 105, 38);
-        border-radius: 6px;
-        cursor: pointer;
-        color: grey;
-    }
+.input-form {
+    width: 400px;
+}
 
-    .form-submit-button:hover {
-        border-color:  rgb(253, 81, 1);
-        color: #fff;
-    }
+.input-data {
+    margin-bottom: 10px;
+    width: 100%;
+    height: 40px;
+    border-color: rgb(248, 105, 38);
+    padding-left: 15px;
+    border-radius: 6px;
+}
+
+.input-data:hover {
+    border-color: rgb(253, 81, 1);
+}
+
+.input-data:focus {
+    outline: none;
+}
+
+.form-submit-button {
+    width: 300px;
+    height: 40px;
+    border-color: rgb(248, 105, 38);
+    border-radius: 6px;
+    cursor: pointer;
+    color: grey;
+}
+
+.form-submit-button:hover {
+    border-color: rgb(253, 81, 1);
+    color: #fff;
+}
 </style>
